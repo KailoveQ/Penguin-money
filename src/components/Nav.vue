eb8c40e85a5b90ed546081b55dd4bd463a16c564
Kai <1114366961@qq.com>
2021-01-19 16:13:44 +0800

    Money页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,13 @@
 <template>
-<div>
-  <div>
+
+  <div id="nav">
     <router-link to="money">记账</router-link>
     |
     <router-link to="labels">标签</router-link>
     |
     <router-link to="statistics">统计</router-link>
   </div>
-</div>
+
 </template>
 
 <script lang="ts">
@@ -17,5 +17,7 @@ name: "Nav"
 </script>
 
 <style lang="scss" scoped>
+.nav{
 
+}
 </style>
