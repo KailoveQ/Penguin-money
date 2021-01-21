3f45149e383a6178f5f64577471f9b9545b5fdbe
Kai <1114366961@qq.com>
2021-01-21 18:35:22 +0800

    基本无完成numberPad组件

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -12,18 +12,7 @@
       <li>食</li>
       <li>住</li>
       <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+
     </ul>
   </div>
 
