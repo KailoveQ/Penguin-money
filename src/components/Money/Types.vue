1830afb5625209206c047ecffc96108cecd2d8df
Kai <1114366961@qq.com>
2021-01-21 17:53:13 +0800

    â完成types组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -12,24 +12,39 @@
 
 </template>
 
-<script lang="js">
-  export default {
-    name: 'Types',
-    data(){
-      return{
-        type:'-', //‘-’代表支出 ‘+’代表输入
+<script lang="ts">
 
-      }
-    },
-    methods:{
-      selectType(type){
-        if(type !== '-'&& type !== '+'){
-          throw new Error('type is unknown')
-        }
-        this.type = type
-      }
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+@Component
+export default class Types extends Vue {
+  type = '-'; // '-'表示支出，'+'表示收入
+  selectType(type: string) {
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unknown');
     }
-  };
+    this.type = type;
+  }
+}
+
+
+  // export default {
+  //   name: 'Types',
+  //   data(){
+  //     return{
+  //       type:'-', //‘-’代表支出 ‘+’代表输入
+  //
+  //     }
+  //   },
+  //   methods:{
+  //     selectType(type){
+  //       if(type !== '-'&& type !== '+'){
+  //         throw new Error('type is unknown')
+  //       }
+  //       this.type = type
+  //     }
+  //   }
+  // };
 </script>
 
 <style lang="scss" scoped>
